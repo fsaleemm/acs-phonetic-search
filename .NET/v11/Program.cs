3b71a4252ab835588770b6b36efba9eed66b50f0
Farhat Saleem <fsaleemms@live.com>
2022-03-07 18:38:13 -0500

    added .NEt SDK example for creating index

---
+++ .NET/v11/Program.cs
@@ -0,0 +1,174 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure;
+using Azure.Search.Documents;
+using Azure.Search.Documents.Indexes;
+using Azure.Search.Documents.Indexes.Models;
+using System.Runtime.CompilerServices;
+
+namespace ACSIndexeCreation
+{
+    class IndexCreator
+    {
+        public static void CreatePhoneticIndex(string indexname, Uri endpoint, string key)
+        {
+            AzureKeyCredential credential = new AzureKeyCredential(key);
+
+            SearchClientOptions options = new SearchClientOptions();
+
+            SearchIndexClient client = new SearchIndexClient(endpoint, credential);
+
+            SearchIndex index = new SearchIndex(indexname)
+            {
+                Fields =
+                {
+                    new SimpleField("id", SearchFieldDataType.String) { IsKey = true, IsFilterable = true, IsSortable = true },
+                    new SearchableField("name") { IsFilterable = true, AnalyzerName = LexicalAnalyzerName.EnMicrosoft },
+                    new SearchableField("phoneticname") { IsFilterable = true, AnalyzerName = "cologne" },
+
+                },
+                TokenFilters =
+                {
+                    new PhoneticTokenFilter("doubleMetaphone")
+                    {
+                        Encoder = PhoneticEncoder.DoubleMetaphone
+                    },
+                    new PhoneticTokenFilter("beiderMorse")
+                    {
+                        Encoder = PhoneticEncoder.BeiderMorse
+                    },
+                    new PhoneticTokenFilter("caverphone1")
+                    {
+                        Encoder = PhoneticEncoder.Caverphone1
+                    },
+                    new PhoneticTokenFilter("caverphone2")
+                    {
+                        Encoder = PhoneticEncoder.Caverphone2
+                    },
+                    new PhoneticTokenFilter("cologne")
+                    {
+                        Encoder = PhoneticEncoder.Cologne
+                    },
+                    new PhoneticTokenFilter("haasePhonetik")
+                    {
+                        Encoder = PhoneticEncoder.HaasePhonetik
+                    },
+                    new PhoneticTokenFilter("koelnerPhonetik")
+                    {
+                        Encoder = PhoneticEncoder.KoelnerPhonetik
+                    },
+                    new PhoneticTokenFilter("metaphone")
+                    {
+                        Encoder = PhoneticEncoder.Metaphone
+                    },
+                    new PhoneticTokenFilter("nysiis")
+                    {
+                        Encoder = PhoneticEncoder.Nysiis
+                    },
+                    new PhoneticTokenFilter("refinedSoundex")
+                    {
+                        Encoder = PhoneticEncoder.RefinedSoundex
+                    },
+                    new PhoneticTokenFilter("soundex")
+                    {
+                        Encoder = PhoneticEncoder.Soundex
+                    }
+
+                },
+                Analyzers =
+                {
+                    new CustomAnalyzer("doubleMetaphone","microsoft_language_tokenizer")
+                    {
+                        TokenFilters = { TokenFilterName.Lowercase, TokenFilterName.AsciiFolding, "doubleMetaphone" }
+                    },
+                    new CustomAnalyzer("beiderMorse","microsoft_language_tokenizer")
+                    {
+                        TokenFilters = { TokenFilterName.Lowercase, TokenFilterName.AsciiFolding, "beiderMorse" }
+                    },
+                    new CustomAnalyzer("caverphone1","microsoft_language_tokenizer")
+                    {
+                        TokenFilters = { TokenFilterName.Lowercase, TokenFilterName.AsciiFolding, "caverphone1" }
+                    },
+                    new CustomAnalyzer("caverphone2","microsoft_language_tokenizer")
+                    {
+                        TokenFilters = { TokenFilterName.Lowercase, TokenFilterName.AsciiFolding, "caverphone2" }
+                    },
+                    new CustomAnalyzer("cologne","microsoft_language_tokenizer")
+                    {
+                        TokenFilters = { TokenFilterName.Lowercase, TokenFilterName.AsciiFolding, "cologne" }
+                    },
+                    new CustomAnalyzer("haasePhonetik","microsoft_language_tokenizer")
+                    {
+                        TokenFilters = { TokenFilterName.Lowercase, TokenFilterName.AsciiFolding, "haasePhonetik" }
+                    },
+                    new CustomAnalyzer("koelnerPhonetik","microsoft_language_tokenizer")
+                    {
+                        TokenFilters = { TokenFilterName.Lowercase, TokenFilterName.AsciiFolding, "koelnerPhonetik" }
+                    },
+                    new CustomAnalyzer("metaphone","microsoft_language_tokenizer")
+                    {
+                        TokenFilters = { TokenFilterName.Lowercase, TokenFilterName.AsciiFolding, "metaphone" }
+                    },
+                    new CustomAnalyzer("nysiis","microsoft_language_tokenizer")
+                    {
+                        TokenFilters = { TokenFilterName.Lowercase, TokenFilterName.AsciiFolding, "nysiis" }
+                    },
+                    new CustomAnalyzer("refinedSoundex","microsoft_language_tokenizer")
+                    {
+                        TokenFilters = { TokenFilterName.Lowercase, TokenFilterName.AsciiFolding, "refinedSoundex" }
+                    },
+                    new CustomAnalyzer("soundex","microsoft_language_tokenizer")
+                    {
+                        TokenFilters = { TokenFilterName.Lowercase, TokenFilterName.AsciiFolding, "soundex" }
+                    }
+                }
+            };
+
+            client.DeleteIndex(indexname);
+            client.CreateIndex(index);            
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string? searchEndpoint = Environment.GetEnvironmentVariable("SEARCH_ENDPOINT");
+                string? key = Environment.GetEnvironmentVariable("SEARCH_API_KEY");
+                string indexname = "phoneticindex";
+
+                if (searchEndpoint == null || key == null)
+                {
+                    Console.WriteLine("Please setup the environment variables SEARCH_ENDPOINT and SEARCH_API_KEY and try again.");
+                    Environment.Exit(-1);
+                }
+                    
+                Uri endpoint = new Uri(searchEndpoint);
+
+                Console.WriteLine("Starting Index Creation: " + indexname);
+
+                //Create an index with a custom Analyzer using the Cologne Phonetic encoder.
+                IndexCreator.CreatePhoneticIndex(indexname, endpoint, key);
+
+                Console.WriteLine("Completed index Creation: " + indexname);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Something went wrong. Exception is: " + ex.Message);
+                Environment.Exit(-1);
+            }
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
---
+++ .NET/v11/README.md
@@ -0,0 +1,57 @@
+# Azure Cognitive Search - Phonetic Search (.NET SDK)
+
+This repo contains scripts to setup an index with phonetic search to help evaluate and benchmark the [phonetic encoders](https://docs.microsoft.com/en-us/dotnet/api/azure.search.documents.indexes.models.phoneticencoder?view=azure-dotnet).
+
+## Pre-work
+
+1. VS Code
+1. Azure Sbuscription is needed.
+1. Setup the [Azure Cognitive Search service](https://docs.microsoft.com/en-us/azure/search/search-create-service-portal).
+
+## Setup the Environment
+
+### Build the .NET solution
+
+In VS Code open terminal
+
+```bash
+dotnet restore
+dotnet build
+```
+
+### Setup Variables
+
+Setup the Cognitive Search connection information environment variables.
+
+If using bash:
+
+```bash
+export SEARCH_ENDPOINT="<Your Azure Cognitive Search Endpoint>"
+export SEARCH_API_KEY="<Your Search API Key>"
+```
+
+If using PowerShell:
+
+```powershell
+$env:SEARCH_ENDPOINT="<Your Azure Cognitive Search Endpoint>"
+$env:SEARCH_API_KEY="<Your Search API Key>"
+```
+
+## Run the Sample Code
+
+Run the Create Phonetic Index script. This script will setup an index with the name provided in the phoneticConfig.py file. Additionally, it will create the 11 analyzers with the phonetic encoding options available.
+
+```bash
+dotnet run
+```
+
+Output:
+
+```bash
+Starting Index Creation: phoneticindex
+Completed index Creation: phoneticindex
+```
+
+Go to the Azure portal, find your Azure Cognitive Search service, go to the indexes and click on the "phoneticindex" and look at the Index Definition, you will see the analyzers setup. See below screenshot for reference.
+
+![index definition](../../images/s1.png)
